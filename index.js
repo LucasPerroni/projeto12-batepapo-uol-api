6f82dd4caab6590da4cc4bc988e592226600b2d9
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-04-27 14:41:02 -0300

    get and post participants

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 import express from "express"
 import cors from "cors"
 import chalk from "chalk"
+import dayjs from "dayjs"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+const participants = []
+const messages = []
+
 //  /participants
 app.get("/participants", (req, res) => {
-  res.send("OK")
+  res.send(participants)
 })
 
 app.post("/participants", (req, res) => {
-  res.send("OK")
+  const { name } = req.body
+  const nameExistent = participants.find((p) => p.name === name)
+
+  if (!name) {
+    res.status(422).send("Name must be filled")
+    return
+  }
+
+  if (nameExistent) {
+    res.status(409).send("This name is already being used")
+    return
+  }
+
+  const time = dayjs().format("HH:mm:ss")
+
+  // add new participant
+  participants.push({
+    name,
+    lastStatus: Date.now(),
+  })
+
+  // add new message to chat
+  messages.push({
+    from: name,
+    to: "Todos",
+    text: "entra na sala...",
+    type: "status",
+    time,
+  })
+
+  res.sendStatus(201)
 })
 
 //  /messages
