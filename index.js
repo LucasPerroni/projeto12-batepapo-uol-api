206d28315ae485d4db4509e83052629354f15e62
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-04-27 21:30:19 -0300

    Store arrays in MongoDB

--- index.js
+++ index.js
@@ -2,64 +2,96 @@ import express from "express"
 import cors from "cors"
 import chalk from "chalk"
 import dayjs from "dayjs"
+import { MongoClient } from "mongodb"
+import dotenv from "dotenv"
 
+dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-const participants = []
-const messages = []
+const client = new MongoClient(process.env.MONGO_URL)
 
 //  /participants
-app.get("/participants", (req, res) => {
-  res.send(participants)
+app.get("/participants", async (req, res) => {
+  try {
+    await client.connect()
+    const db = client.db("projeto12")
+    const participants = await db.collection("participants").find({}).toArray()
+
+    res.send(participants)
+  } catch (e) {
+    res.sendStatus(500)
+  } finally {
+    client.close()
+  }
 })
 
-app.post("/participants", (req, res) => {
-  const { name } = req.body
-  const nameExistent = participants.find((p) => p.name === name)
+app.post("/participants", async (req, res) => {
+  try {
+    await client.connect()
+    const db = client.db("projeto12")
 
-  if (!name) {
-    res.status(422).send("Name must be filled")
-    return
+    const { name } = req.body
+    const nameExistent = await db.collection("participants").find({ name: name }).toArray()
+
+    // validate if name has been filled
+    if (!name) {
+      res.status(422).send("Name must be filled")
+      return
+    }
+
+    // validate if name already exists
+    if (nameExistent.length > 0) {
+      res.status(409).send("This name is already being used")
+      return
+    }
+
+    // push new user to "participants" array
+    await db.collection("participants").insertOne({
+      name,
+      lastStatus: Date.now(),
+    })
+
+    // push new user to "messages" array
+    const time = dayjs().format("HH:mm:ss")
+    await db.collection("messages").insertOne({
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time,
+    })
+
+    res.sendStatus(201)
+  } catch (e) {
+    res.sendStatus(500)
+  } finally {
+    client.close()
   }
-
-  if (nameExistent) {
-    res.status(409).send("This name is already being used")
-    return
-  }
-
-  const time = dayjs().format("HH:mm:ss")
-
-  // add new participant
-  participants.push({
-    name,
-    lastStatus: Date.now(),
-  })
-
-  // add new message to chat
-  messages.push({
-    from: name,
-    to: "Todos",
-    text: "entra na sala...",
-    type: "status",
-    time,
-  })
-
-  res.sendStatus(201)
 })
 
 //  /messages
-app.get("/messages", (req, res) => {
-  res.send("OK")
+app.get("/messages", async (req, res) => {
+  try {
+    await client.connect()
+    const db = client.db("projeto12")
+    const messages = await db.collection("messages").find({}).toArray()
+
+    res.send(messages)
+  } catch (e) {
+    res.sendStatus(500)
+  } finally {
+    client.close()
+  }
 })
 
-app.post("/messages", (req, res) => {
+app.post("/messages", async (req, res) => {
   res.send("OK")
 })
 
 //  /status
-app.post("/status", (req, res) => {
+app.post("/status", async (req, res) => {
   res.send("OK")
 })
 
